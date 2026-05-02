6b3e3e15fbcda19b5f0579dbc7a44d6fb35b5bce
CasTolax <cantuncer32@gmail.com>
2026-05-02 10:47:57 +0300

    Enhance CPU loop with command handling and includes
    Added command handling in the CPU loop and included necessary headers.

--- 8bit_cpu_simu/Epsilon_cpu/src/cpu/cpu.c
+++ 8bit_cpu_simu/Epsilon_cpu/src/cpu/cpu.c
@@ -1,7 +1,5 @@
 /*  
-    * By CasTolax 2026
-    * The CPU loop is here
-    
+    By CasTolax 2026
 */
 
 #include <stdint.h>
@@ -14,8 +12,30 @@
 #include "Epsilon_cpu/src/lib/opcodes_map.h"
 #include "Epsilon_cpu/src/lib/commands/commands.h"
 #include "Epsilon_cpu/src/lib/alu.h"
+#include "Epsilon_cpu/src/lib/commands/write.h"
+#include "Epsilon_cpu/src/lib/commands/hlt.h"
+#include "Epsilon_cpu/src/lib/commands/lda.h"
+#include "Epsilon_cpu/src/lib/commands/ldb.h"
+#include "Epsilon_cpu/src/lib/commands/read.h"
+#include "Epsilon_cpu/src/lib/commands/push.h"
 
 int cpu_main(void)
 {
+    // loop + control
+    
+    while (SLEEP(1));
+    {
+        switch (commands)
+        {
+           // There someting is wrong, I am not sure about that!
+        case LDA:
+            ADD = RAM[PC++];
+            AR = RAM[ADD];
+            break;
+        
+        default:
+            break;
+        }
+    }
     
 }
