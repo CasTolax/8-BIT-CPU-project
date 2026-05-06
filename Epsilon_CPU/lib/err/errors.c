540fe2ed11918f2ba317a8e015eb423fe97d0c7c
CasTolax <cantuncer32@gmail.com>
2026-05-06 10:19:16 +0300

    Update errors.c

--- Epsilon_CPU/lib/err/errors.c
+++ Epsilon_CPU/lib/err/errors.c
@@ -7,23 +7,29 @@
 #include <stdlib.h>
 #include "errors.h"
 
-// Sıfıra bölme hatası
-// Kesir hesaplamalarında bölen sıfır olduğunda bu fonksiyon çağrılır.
+// Division by zero error
+// This function is called when the divisor is zero in fractional calculations.
 void zero_division_error(void) {
     fprintf(stderr, "ERROR: Division by zero!\n");
     exit(EXIT_FAILURE);
 }
 
-// Geçersiz işlem hatası
-// CPU geçersiz bir işlem kodu çalıştırmaya çalışırsa bu fonksiyon çağrılır.
+// Invalid instruction error
+// This function is called if the CPU attempts to execute an invalid instruction code.
 void invalid_process_error(void) {
     fprintf(stderr, "ERROR: Invalid process/instruction!\n");
     exit(EXIT_FAILURE);
 }
 
-// Bellek taşması hatası
-// RAM'in sınırı aşılırsa bu fonksiyon çağrılır.
+// Out-of-memory error
+// This function is called if the RAM limit is exceeded.
 void memory_overflow_error(void) {
     fprintf(stderr, "ERROR: Memory overflow!\n");
+    exit(EXIT_FAILURE);
+}
+
+// if, there is a NULL or empyt variable and value, this function will be called
+void null_warning(void){
+    fprintf(stderr, "WARNING: there is a NULL output or input variable, writing in RAM... \n");
     exit(EXIT_FAILURE);
 }
