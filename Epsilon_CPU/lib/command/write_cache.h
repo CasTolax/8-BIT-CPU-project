1732c8be18e98c7063245de99dd176c36a7c569f
CasTolax <cantuncer32@gmail.com>
2026-05-11 08:43:15 +0300

    Update write_cache.h with cache size definition

--- Epsilon_CPU/lib/command/write_cache.h
+++ Epsilon_CPU/lib/command/write_cache.h
@@ -8,9 +8,11 @@
 #ifndef WRITE_CACHE
 #define WRITE_CACHE
 
-#define WRITE_CACHE_FALSE // yazdırılmayacak ise hayır
-#define WRITE_CACHE_TRUE  // yazdırılacak ise evet
+#define WRITE_CACHE_FALSE // 0 yazdırılmayacak ise hayır
+#define WRITE_CACHE_TRUE  // 1 yazdırılacak ise evet
 
-void write_cache(CACHE_SIZE);
+#define CACHE_SIZE	64 // size of cache(64kb)
+
+void write_cache(int write_process);
 
 #endif
