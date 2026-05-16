0b1df2d59e378dac2230ababa7890a506edde52b
CasTolax <cantuncer32@gmail.com>
2026-05-16 09:11:33 +0300

    Create keyboard_interr.h

---
+++ Epsilon_CPU/lib/interrupts/keyboard_interr.h
@@ -0,0 +1,17 @@
+/* 
+	By CasTolax 2026
+*/
+
+#include <stdint.h>
+
+#ifndef KEYBOARD_INTERR_H
+#define KEYBOARD_INTERR_H
+
+#define KEYBOARD_TRUE 1
+#define KEYBOARD_FALSE 0
+
+int keyboard_interrupts (void);
+
+#endif
+
+
