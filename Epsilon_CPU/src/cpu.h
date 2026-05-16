72689360a2b2d1016a6caffe5df2ceb60b150326
CasTolax <cantuncer32@gmail.com>
2026-05-16 09:10:24 +0300

    Update cpu.h

--- Epsilon_CPU/src/cpu.h
+++ Epsilon_CPU/src/cpu.h
@@ -6,15 +6,21 @@
 #define CPU_H
 
 #include <stdint.h>
+
 /* main function */
 int CPU_MAIN(void);
+
 /* error handling for CPU */
 int cpu_error(void);
 
-// scheduler
+/* scheduler */
 int scheduler_cpu(void);
 
 /* cache */
 int cache_cpu(void);
+
+/* Keyboard İnterrupt */
+int keyboard_interrupt (void);
+// gel daha sonra bu fonksiyonu yaz
 
 #endif
