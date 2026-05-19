36ad36a755d5704469275dcefe4974f7361f6da2
CasTolax <cantuncer32@gmail.com>
2026-05-19 11:16:36 +0300

    Update clock.c

--- Epsilon_CPU/lib/clock.c
+++ Epsilon_CPU/lib/clock.c
@@ -3,12 +3,16 @@
 */
 
 #include <stdint.h>
-#include "clock.h"
 #include <unistd.h>
+#include <stdbool.h>
+
+#include "clock.h"
 #include "opcodes.h"
 
 uint8_t PC = 0;
 int RAM[RAM_SIZE] = {0};
+bool clock_active = true;
+
 
 // clock cycle function
 void clock_cycle(void){
