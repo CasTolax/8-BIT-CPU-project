f98db02e4027d7387d39e040b73ebfa95376c53d
CasTolax <cantuncer32@gmail.com>
2026-05-19 12:23:06 +0300

    Rearrange opcode definitions in opcodes.h

--- Epsilon_CPU/lib/opcodes.h
+++ Epsilon_CPU/lib/opcodes.h
@@ -11,10 +11,13 @@
 extern int RAM[RAM_SIZE];    // RAM array
 
 #define CPU_START 1    // start of the CPU
-#define add 0x01            // addition
-#define sub 0x02            // subtraction
-#define mul 0x03            // multiplication
-#define div 0x04            // division
+#define reserved  0x0ff
+#define add 0x01       // addition
+#define sub 0x02       // subtraction
+#define mul 0x03       // multiplication
+#define div 0x04       // division
 
 
+// I/0
+#define sys_status 0xBe1 // system status
 #endif 
