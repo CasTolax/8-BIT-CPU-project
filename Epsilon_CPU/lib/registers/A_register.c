cea6fcfda0be110b8ff3dc3be3069918206a0720
CasTolax <cantuncer32@gmail.com>
2026-05-21 09:30:51 +0300

    Refactor ARegister and AWait functions
    Refactor ARegister function to improve readability and structure.

--- Epsilon_CPU/lib/registers/A_register.c
+++ Epsilon_CPU/lib/registers/A_register.c
@@ -1,5 +1,5 @@
 /*
-	By CasTolax 2026
+    By CasTolax 2026
 */
 
 #include <stdint.h>
@@ -7,38 +7,60 @@
 #include <stdlib.h>
 
 #include "../lib/registers/A_register.h"
-
 #include "../lib/alu/alu.h"
 #include "../lib/command/write_cache.h"
 #include "../lib/opcodes.h"
+#include "../lib/clock.h"
 
+void AWait(int index, int AREGISTER[AR_SIZE]);
 
 void ARegister(int AREGISTER[AR_SIZE])
 {
-	//int WRITING = 0; // optional
+    /* Transfer data */
+    for (int i = 0; i < AR_SIZE; i++)
+    {
+        RAM[i] = AREGISTER[i];
+        CACHE[i] = AREGISTER[i];
+    }
 
-	
-	/* Get data from RAM */
+    /* Print register contents */
+    printf("A REGISTER:\n");
 
-	for(int i = 0; i<8; i++)
-	{
-		RAM[i] = AREGISTER[i];
+    for (int i = 0; i < AR_SIZE; i++)
+    {
+        if (i % 4 == 0)
+        {
+            printf("\n%02d: ", i);
+        }
 
-		CACHE[i] = AREGISTER[i];
+        printf("%2d ", AREGISTER[i]);
+    }
 
-		if(i % 4 == 0)
-		{
-			printf("\n%02d: ", i);
-		}
-		
-		printf("%2d ", AREGISTER[i]);
-	}
-	printf("\n");
+    printf("\n");
 
-	
+    /* Simulate register lifecycle */
+    for (int i = 0; i < AR_SIZE; i++)
+    {
+        AWait(i, AREGISTER);
+    }
+}
 
-	
+void AWait(int index, int AREGISTER[AR_SIZE])
+{
+    /*
+        First half:
+        clear temporary register data
 
+        Second half:
+        wait clock cycle
+    */
 
-
+    if (index < (AR_SIZE / 2))
+    {
+        AREGISTER[index] = 0;
+    }
+    else
+    {
+        clock_cycle();
+    }
 }
