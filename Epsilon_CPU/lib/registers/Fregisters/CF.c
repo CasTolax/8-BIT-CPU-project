9f81409bb3e85b78c887a4c9fbb9aa4d84779d6a
CasTolax <cantuncer32@gmail.com>
2026-05-22 09:36:45 +0300

    Add files via upload

---
+++ Epsilon_CPU/lib/registers/Fregisters/CF.c
@@ -0,0 +1,13 @@
+/*
+	By CasTolax 2026
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void CF(int Carry_flag)
+{
+	Carry_flag = 1;
+	printf("carry flag test = CF: %d\n",Carry_flag );
+}
