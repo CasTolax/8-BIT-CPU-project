bc190bd7a405bbcf24b9d93ba091405b73b84d6e
CasTolax <cantuncer32@gmail.com>
2026-05-29 10:27:05 +0300

    Update halt_process.c

--- 8-bit_cpu/Epsilon_CPU/lib/PANIC/halt_process.c
+++ 8-bit_cpu/Epsilon_CPU/lib/PANIC/halt_process.c
@@ -1,5 +1,8 @@
 /*
 	By CasTolax 2026
+
+    I solve this shit bugs and errors.
+    thanks cloude!
 */
 
 #include <stdint.h>
@@ -15,46 +18,53 @@
 #include "../lib/opcodes.h"
 #include "../lib/clock.h"
 
-uint8_t sys_control_handling(void)
+int sys_control_handling(void)
 {
     uint8_t cores[10];
     memset(cores, 0, sizeof(cores));
-
     panic_datas_cpu_t p = {0};
 
     /* RAM CONTROL */
-    if (RAM[RAM_SIZE - 1] == 255)
+    int ram_full = 1;
+    for (int i = 0; i < RAM_SIZE; i++) {
+        if (RAM[i] == 0) { ram_full = 0; break; }
+    }
+
+    if (ram_full)
     {
         p.RAM_HANDLING = 1;
         cores[0] = 1;
-
         memory_overflow_error();
         clock_cycle();
         RAM_RESET();
-
         print_ERROR("RAM OVERFLOW ERROR[0x0ddf]");
         printf("RAM ERROR\n");
         fflush(stdout);
-
-        return p.RAM_HANDLING;
     }
 
     /* CACHE CONTROL */
-    if (CACHE[CACHE_SIZE - 1] == 64)
+    int cache_full = 1;
+    for (int i = 0; i < CACHE_SIZE; i++) {
+        if (CACHE[i] == 0) { cache_full = 0; break; }
+    }
+
+    if (cache_full)
     {
         p.CACHE_HANDLING = 1;
         cores[1] = 1;
-
         cache_overflow_error();
         clock_cycle();
-
         print_ERROR("CACHE OVERFLOW ERROR[0x0AAf]");
         printf("CACHE ERROR\n");
         fflush(stdout);
-
-        return p.CACHE_HANDLING;
     }
 
-    
+    /* this is the debuging*/
+    printf("[PANIC CTRL] RAM_HANDLING=%d | CACHE_HANDLING=%d\n",
+           p.RAM_HANDLING, p.CACHE_HANDLING);
+    fflush(stdout);
+
+    if (p.RAM_HANDLING) return p.RAM_HANDLING;
+    if (p.CACHE_HANDLING) return p.CACHE_HANDLING;
     return 0;
 }
